a54e1afd458fc2458b8ba1be39a5234a44ea7fef
Priyanshu Nayan <priyanshunayan456@gmail.com>
2021-01-23 19:48:58 +0530

    hover text done ☑️

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -25,29 +25,14 @@ export default {
             }
         },
         methods:{
-            // updateDrawing(){
-            //     const context = document.getElementById('main-canvas').getContext('2d');
-            //     context.clearRect(0, 0, document.getElementById('main-canvas').width, document.getElementById('main-canvas').height);
-            //     paper.setup(document.getElementById('main-canvas'))
-            //     const point = new paper.Point(this.x, this.y)
-            //     const circle = new paper.Path.Circle(point, this.circle_diameter/2)
-            //     circle.fillColor = 'grey'
-            //     circle.strokeColor = 'black'
-            // },
             paintImage(){
-
                 this.images.forEach((image, index) => {
-                    //const ctx = document.getElementById(String(index+1)).getContext('2d');
-                   
-                    // img.src = `${this.publicUrl}static/${image.file_name}`;
-                    const canvas = document.getElementById(String(index+1));
-                    
+                    const canvas = document.getElementById(String(index+1)); 
                     canvas.width = 500;
                     canvas.height = 500;
                     const ratioX = 500 / image['width'];
                     const ratioY = 500/ image['height'];
                     paper.setup(canvas);
-                    //console.log(canvas.width, canvas.height)
                     const raster = new paper.Raster({
                             source: `${this.publicUrl}static/${image.file_name}`,
                     });
@@ -62,11 +47,25 @@ export default {
                             const point1 = new paper.Point(bbox['bbox'][0]*ratioX, bbox['bbox'][1]*ratioY);
                             const point2 = new paper.Size(bbox['bbox'][2]*ratioX, bbox['bbox'][3]*ratioY);
                             const rectangle = new paper.Path.Rectangle(point1, point2);
-                            rectangle.on('click', () => {
-                                console.log(bbox['name'], "hovered");
-                            })
-                            rectangle.fillColor = bbox['color'];
-                            rectangle.strokeColor = 'black';
+
+                            const text = new paper.PointText(bbox['bbox'][0]*ratioX, bbox['bbox'][1]*ratioY);
+                            text.justification = 'center';
+                            text.fillColor = 'black';
+                            text.content = bbox['name'];
+                            text.visible = false;
+                            
+                            rectangle.onMouseEnter =  () => {// Layout the tooltip above the dot
+                                console.log("eneters");
+                                text.visible = true;
+                              
+                            };
+                            rectangle.onMouseLeave = () => {
+                                console.log("leave");
+                                text.visible = false;
+                            
+                            };
+                            
+                            rectangle.strokeColor = bbox['color'];
                         })    
                 });
 
@@ -83,28 +82,12 @@ export default {
                 });
                 return result;
             },
-            boundLabel(){
-                // this.images.forEach((image, index) => {
-        
-                //         const point1 = new paper.Point(0, 0);
-                //         console.log(point1);
-                //         const point2 = new paper.Point(100, 100);
-                //         console.log(point2);
-                //         const rectangle = new paper.Path.Rectangle(point1, point2)
-                //         rectangle.fillColor = 'grey'
-                //         rectangle.strokeColor = 'black'
-                    
-                // });
-
-
-            },
             getPath(file_name){
                 return `${this.publicUrl}static/${file_name}`
             },
         },
         mounted(){
             this.paintImage();
-            //this.boundLabel();
         }
     }
 </script>
